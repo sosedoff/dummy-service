37e4a8a4ab21c0812e9cfaed1b0ba5d4cadffbd8
Dan Sosedoff <dan.sosedoff@gmail.com>
2015-09-02 15:21:18 -0500

    Add request counter and include environment into / endpoint

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "sinatra"
 require "json"
 require "securerandom"
 
+$request_counter = 0
+
 backend_id = SecureRandom.uuid
 
 def json(data)
@@ -9,12 +11,18 @@ def json(data)
 end
 
 before do
+  $request_counter += 1
+
   headers["X-Backend-Id"] = backend_id
   content_type :json, encoding: "utf-8"
 end
 
 get "/" do
-  json backend: backend_id
+  json(
+    backend: backend_id,
+    request_counter: $request_counter,
+    env: Hash[ENV.to_hash.sort_by { |k, _| k }]
+  )
 end
 
 get "/exception" do
